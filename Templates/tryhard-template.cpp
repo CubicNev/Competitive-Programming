16057ca4dcf1519e30204592858eb5c3e15c67c4
CubicNev <juan.nevtor@hotmail.com>
2024-05-15 09:17:35 -0600

    Completando plantillas

--- Templates/tryhard-template.cpp
+++ Templates/tryhard-template.cpp
@@ -6,4 +6,33 @@
 
 # include <iostream>
 # include <bits/stdc++.h>
+
 # define forn(i,n) for(int i=0; i<int(n);i++)
+# define forr(i,a,b) for(int i=int(a); i <= int(b); i++)
+# define fore(obj) for(const auto& element: obj)
+# define endl "\n"
+
+typedef long long ll;
+
+// Best practices
+using std::cin;
+using std::count;
+using std::string;
+using std::ios_base;
+
+void solution() {
+    // Solve here
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int t = 1;
+    // cin >> t;
+
+    while(t--){
+        solution();
+    }
+
+    return 0;
+}
